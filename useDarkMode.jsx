be98c6cca77a112f5f6cac82c79c65fe7c7ee51d
plazadevina <plazadevina@gmail.com>
2025-08-08 17:00:16 +0000

    git commit -m "Code-lab-web"

---
+++ useDarkMode.jsx
@@ -0,0 +1,14 @@
+};
+const use DarkMode =() => {
+    const [enabled, setEnabled]=useLocalStorage('dark-theme');
+    const isEnabled = typeof enabledState ===
+    'undefined' && enabled;
+    useEffect(()) => }
+    const className = 'dark';
+    const bodyClass = window.document.body.classList;
+    isEnabled ? bodyclass.add(className):bodyclass.remove(className);
+},[enabled, isEnabled]);
+return [enabled, setEnabled]
+};
+export default useDarkMode; 
+}
