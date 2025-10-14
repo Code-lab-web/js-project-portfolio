bb5aae978af928bc468a921b9b8e2e853e8350e6
plazadevina <plazadevina@gmail.com>
2025-10-14 10:25:38 +0000

    git commit -m "Code-lab-web"

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var logger = require('morgan');
+var session = require('express-session');
+var session = require('express-session');
+var passport = require('passport');
+var SQLiteStore = require('connect-sqlite3')(session);
 var session = require('express-session');
 var passport = require('passport');
 var SQLiteStore = require('connect-sqlite3')(session);
 var indexRouter = require('./routes/index');
+
 var authRouter = require('./routes/auth');
 
 var app = express();
@@ -23,8 +29,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/', indexRouter);
+app.use('/', authRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
